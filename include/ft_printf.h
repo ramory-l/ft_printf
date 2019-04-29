983cb8be3574eefba7cb67fdd413d98c4583191f
Roxane Amory lorch <ramory-l@mi-b3.21-school.ru>
2019-04-29 16:41:09 +0300

    added ft_printf

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -2,6 +2,7 @@
 #define FT_PRINT_F_H
 
 #include "get_next_line.h"
+#include <stdarg.h>
 
 int max(int a, int b);
 
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,9 +1,25 @@
 #include "ft_printf.h"
 
-int max(int a, int b)
+int	ft_printf(const char *format, ...)
 {
-	if (a > b)
-		return (a);
-	else
-		return (b);
+	size_t	i;
+	int		d;
+	va_list ap;
+
+	i = 0;
+	va_start(ap, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'd')
+			{
+				d = va_arg(ap, int);
+				ft_print_int(d);
+			}
+		}
+		i++;
+	}
+	va_end(ap);
+	return (0);
 }
