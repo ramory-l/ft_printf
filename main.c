4c41b2582a6e4339829b5b9e95de7591f9497a3a
Iron emmett Dunaver <idunaver@mi-c3.21-school.ru>
2019-05-16 19:46:12 +0300

    Заново long_double

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include "ft_printf.h"
 #include <stdio.h>
+#include <float.h>
+#include <limits.h>
 
 int 	main(void)
 {
 	// float flt;
-	// double dbl;
+	double dbl;
 	long double l_dbl;
 	
 	// flt = 11.11;
-	// dbl = 21.21;
-	l_dbl = 42.42;
+	// dbl = 0.5;
+	l_dbl = 42.0;
 	// printf("custom float: \n");
 	// ft_double(flt);
 	// printf("\noriginal float: \n%.49f\n\n", flt);
 	// printf("custom double: \n");
 	// ft_double(dbl);
-	// printf("\noriginal double: \n%.48f\n\n", dbl);
-	printf("custom long double: \n");
+	// printf("\noriginal double: \n%.1500f\n\n", dbl);
+	// printf("exp: %d\n", DBL_MAX_EXP);
+	// printf("custom long double: \n");
+	// ft_long_double(l_dbl);
+	// printf("\noriginal long double: \n%.58Lf\n", l_dbl);
 	ft_long_double(l_dbl);
-	printf("\noriginal long double: \n%.58Lf\n", l_dbl);
 	return (0);
 }
--- src/ft_long_double.c
+++ src/ft_long_double.c
@@ -1,145 +1,32 @@
-#include "ft_printf.h"
 #include <stdio.h>
+#include "ft_printf.h"
+#include <float.h> // библиотека с константами
 
-static ss_array	*ft_bzero_array(ss_array *double_a)
+typedef union
 {
-	double_a->len_arr = 35000;
-	double_a->tmp = 0;
-	while (double_a->len_arr--)
-	{
-		double_a->mantis_arr[double_a->tmp] = 0;
-		double_a->tmp++;
-	}
-	double_a->len_arr = 35000;
-	double_a->tmp = 0;
-	return (double_a);
-}
+	__uint128_t uint128t;
+	long double long_double;
+}				longdoubletouint128;
 
-static ss_array	*ft_mantis_plus_array(ss_array *double_a, long long mantis)
+struct	sem_long_double
 {
-	while (mantis)
-	{
-		double_a->len_arr--;
-		double_a->mantis_arr[double_a->len_arr] = mantis % 10;
-		mantis /= 10;
-	}
-	double_a->len_arr = 35000;
-	double_a->tmp = 0;
-	return (double_a);
-}
+	int sign;
+	int exp;
+	long long mantis;
+};
 
-static ss_array	*ft_multandfill(ss_array *double_a)
+void    ft_long_double(long double number)
 {
-	double_a->mantis_arr[double_a->len_arr] *= double_a->base;
-	double_a->mantis_arr[double_a->len_arr] += double_a->tmp;
-	if (double_a->mantis_arr[double_a->len_arr] > 9)
-	{
-		double_a->tmp = double_a->mantis_arr[double_a->len_arr] / 10;
-		double_a->mantis_arr[double_a->len_arr] %= 10;
-	}
-	else
-		double_a->tmp = 0;
-	return (double_a);
-}
-
-static ss_array	*ft_more_than_nine(ss_array *double_a)
-{
-	if (double_a->tmp > 0)
-		double_a = ft_multandfill(double_a);
-	else
-	{
-		double_a->tmp = (double_a->mantis_arr[double_a->len_arr]
-		* double_a->base) / 10;
-		double_a->mantis_arr[double_a->len_arr] =
-		(double_a->mantis_arr[double_a->len_arr] * double_a->base) % 10;
-	}
-	if (double_a->mantis_arr[double_a->len_arr - 1] == 0)
-	{
-		double_a->mantis_arr[double_a->len_arr - 1] = double_a->tmp;
-		double_a->tmp = 0;
-		double_a->len_arr--;
-	}
-	return (double_a);
-}
-
-static ss_array	*ft_mult_column(int exp, ss_array *double_a)
-{
-	while (exp)
-	{
-		double_a->tmp = 0;
-		double_a->len_arr = 35000;
-		while (double_a->len_arr)
-		{
-			double_a->len_arr--;
-			if ((double_a->mantis_arr[double_a->len_arr] * double_a->base) > 9)
-				double_a = ft_more_than_nine(double_a);
-			else
-				double_a = ft_multandfill(double_a);
-			if (double_a->mantis_arr[double_a->len_arr])
-				double_a->temp = double_a->len_arr;
-		}
-		exp--;
-	}
-	double_a->len_arr = 35000;
-	return (double_a);
-}
-
-static void		ft_print_double(ss_array *double_a, int exp)
-{
-	char c;
-
-	if (double_a->base == 5)
-	{
-		while (double_a->temp != 35000)
-		{
-			if (double_a->temp == (double_a->len_arr - exp))
-				write(1, ".", 1);
-			c = double_a->mantis_arr[double_a->temp] + '0';
-			write(1, &c, 1);
-			double_a->temp++;
-		}
-	}
-	else
-	{
-		while (double_a->len_arr != 35000)
-		{
-			c = double_a->mantis_arr[double_a->len_arr] + '0';
-			write(1, &c, 1);
-			double_a->len_arr--;
-		}
-	}
-}
-
-static void		ft_itoa_and_print_longdouble(long long mantis, int exp)
-{
-	ss_array *double_a;
-
-	double_a = (ss_array*)malloc(sizeof(ss_array));
-	double_a = ft_bzero_array(double_a);
-	double_a = ft_mantis_plus_array(double_a, mantis);
-	double_a->base = exp <= 16446 ? 5 : 2;
-	exp = 16446 - exp;
-	double_a = ft_mult_column(exp, double_a);
-	ft_print_double(double_a, exp);
-    free(double_a);
-}
-
-void			ft_long_double(long double nbr)
-{
-	struct sem_long_double longdbl;
-	lngdbltouint128 bits;
-
-	longdbl.sign = 0;
-	longdbl.exp = 0;
-	longdbl.mantis = 0;
-	bits.l_dbl = nbr;
-	longdbl.sign = (((bits.u_int128 >> (80 - 1)) & 0x1) == 0) ? 1 : -1;
-	longdbl.exp = (int)((bits.u_int128 >> (80 - 1 - 15)) & 0x7fffL);
-	longdbl.mantis = bits.u_int128 & 0xFFFFFFFFFFFFFFFF;
-    printf("sign: %d\n", longdbl.sign);
-    printf("exp: %d\n", longdbl.exp);
-    printf("mantis: %llu\n", longdbl.mantis);
-	if (longdbl.sign == -1)
+    longdoubletouint128 bits;
+    struct sem_long_double longdouble;
+    
+    bits.long_double = number;
+    longdouble.sign = (((bits.uint128t >> (80 - 1)) & 0x1) == 0) ? 1 : -1;
+    if (longdouble.sign == -1)
         write(1, "-", 1);
-	ft_itoa_and_print_longdouble(longdbl.mantis, longdbl.exp);
+    longdouble.exp = (int)((bits.uint128t >> 64) & 0x7fffL);
+    longdouble.mantis = bits.uint128t & 0xFFFFFFFFFFFFFFFF;
+    printf("sign: %d\n", longdouble.sign);
+    printf("exp: %d\n", longdouble.exp);
+    printf("mantis: %llu\n", longdouble.mantis);
 }
