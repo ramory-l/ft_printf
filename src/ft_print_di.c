cd8a23a5e77e5ca972424ec4c6e4693d43302671
Roxane Amory Lorch <ramory.lorch@icloud.com>
2019-06-03 22:18:18 +0300

    начал делать знаковые числа

--- src/ft_print_di.c
+++ src/ft_print_di.c
@@ -1,6 +1,38 @@
 #include "ft_printf.h"
 
+static void	ft_plus_width(t_printf *data, t_buffer *bf)
+{
+	if ((data->flags & FLAG_SPACE || data->width > data->len) &&
+		!(data->flags & FLAG_ZERO))
+	{
+		if (data->width > data->len)
+			ft_fill_bf_spaces(data, bf);
+		else
+		{
+			if (bf->temp[0] != '-')
+			{
+				bf->buffer[bf->s] = ' ';
+				bf->s++;
+				ft_check_buffer(data, bf);
+			}
+		}
+	}
+	if (data->width > data->len && data->flags & FLAG_ZERO)
+	{
+		if (data->flags & FLAG_PLUS)
+			data->len++;
+		ft_fill_bf_zeroes(data, bf);
+	}
+}
+
 void		ft_print_di(t_printf *data, t_buffer *bf)
 {
+	if (data->flags & FLAG_PLUS && !data->sign && bf->temp[0] != '-')
+	{
+		bf->buffer[bf->s] = '+';
+		bf->s++;
+		ft_check_buffer(data, bf);
+	}
+	ft_plus_width(data, bf);
 	ft_fill_bf(data, bf);
 }
